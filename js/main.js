35c3c7e0296b5ac9f1c7a9b12fb29cbe85c71115
ahoyland <alex.hoyland@gmail.com>
2016-04-08 15:58:02 -0400

    fuck your commit message

--- js/main.js
+++ js/main.js
@@ -1,23 +1,44 @@
-var app = {
-
-    findByName: function() {
+var app = 
+{
+    findByName: function() 
+    {
         console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
+        this.store.findByName($('.search-key').val(), function(employees) 
+        {
             var l = employees.length;
             var e;
             $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
+            for (var i=0; i<l; i++) 
+            {
                 e = employees[i];
                 $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
             }
         });
     },
 
-    initialize: function() {
-        this.store = new LocalStorageStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    showAlert: function (message, title) 
+    {
+        if (navigator.notification) 
+        {
+            navigator.notification.alert(message, null, title, 'OK');
+        } 
+        else 
+        {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+    
+    initialize: function() 
+    {
+        var self = this;
+        this.store = new MemoryStore(
+            function() 
+            {
+                self.showAlert('Store Initialized', 'Info');
+            }
+        );
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));   
     }
-
 };
 
 app.initialize();
